58c9b63f69fc2bb9ac1ce2e43ece33f27f51e7fe
stvhoey <stvhoey.vanhoey@ugent.be>
2014-11-11 22:19:11 +0100

    added first test-script

---
+++ tests.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 10 12:04:27 2014
+
+@author: stvhoey
+"""
+
+import numpy as np
+import pandas as pd
+
+
+from baseflow import *
+from flowanalysis import HydroAnalysis
+
+flowdata = pd.read_pickle("FlowData")
+raindata = pd.read_pickle("RainData")
+
+flow2use = flowdata["L06_347"]
+
+temp = HydroAnalysis(flowdata)#, datacols=['LS06_342'])
+
+
+#! test for both single column as multicolumn
