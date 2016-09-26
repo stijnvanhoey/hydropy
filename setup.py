5a0d97ed5cb809d72be281856e4895c958909a15
mroberge <mroberge.whois@gmail.com>
2016-09-25 20:55:13 -0400

    Added python 3.5 support
    modified print to print();
    added from __future__ import print_function;
    modified imports to relative imports;
    added from __future__ import absolute_import
    modified StringIO to io.StringIO
    added manifest file so that data directory gets packaged too. (The
    manifest file also lists some things that might be nice to include in
    the future)
    Added 'include_package_data' line to setup.py so that setup looks for
    MANIFEST.in
    Added python 3.5 classifier to setup.py

--- setup.py
+++ setup.py
@@ -9,5 +9,16 @@ setup(name='hydropy',
       author='Stijn Van Hoey',
 	  author_email='stvhoey.vanhoey@ugent.be',
 	  #license=' ',
+       include_package_data=True,
 	  packages=['hydropy'],
-	  keywords='hydrology time series hydroTSM',)
+	  keywords='hydrology time series hydroTSM',
+       classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Scientific/Engineering :: Information Analysis',
+        'Topic :: Scientific/Engineering :: Visualization',
+        'Topic :: Utilities'
+    ],)
