61a43a8bc0c009909abb37a6ec6ec363b6ca8749
mroberge <mroberge.whois@gmail.com>
2016-10-02 22:55:45 -0400

    PEP8 compliance
    I added and removed spaces to match PEP8 as best as I could. It passed
    the tests for python 2.7 and 3.5, but since there aren't many tests,
    this is really just a test of whether it will build. I also ran the
    Ipython notebook as a test.

--- hydropy/reading_third_party_data.py
+++ hydropy/reading_third_party_data.py
@@ -32,24 +32,24 @@ def load_VMM_zrx_timeserie(filename):
     """
     zrxf = open(filename, 'r')
 
-    #define header length (= number of lines with starting #)
+    # define header length (= number of lines with starting #)
     ctestall = zrxf
     headerlength = 0
     ctest = '#'
     while ctest == '#':
         ctest = ctestall.readline()[0]
         headerlength += 1
-    print('File ', filename.split("\\")[-1],\
-            ' headerlength is: ', headerlength-1)
+    print('File ', filename.split("\\")[-1],
+          ' headerlength is: ', headerlength-1)
     zrxf.close()
 
-    #Read the data
+    # Read the data
     data = pd.read_table(filename, sep=' ',
-                       skiprows=headerlength-1, index_col=0,
-                       parse_dates=True,
-                       header=None, usecols=(0, 1),
-                       names=['Time', filename.split("\\")[-1][:-4]],
-                       na_values='-777.0')
+                         skiprows=headerlength-1, index_col=0,
+                         parse_dates=True,
+                         header=None, usecols=(0, 1),
+                         names=['Time', filename.split("\\")[-1][:-4]],
+                         na_values='-777.0')
     return data
 
 
@@ -63,7 +63,7 @@ def _gettext(ftp, filename, outfile=None):
     if outfile is None:
         outfile = sys.stdout
     # use a lambda to add newlines to the lines read from the server
-    ftp.retrlines("RETR " + filename, lambda s, w = outfile.write: w(s+"\n"))
+    ftp.retrlines("RETR " + filename, lambda s, w=outfile.write: w(s+"\n"))
 
 
 def load_VMM_zrx_timeseries_from_ftp(server, login, password,
@@ -92,9 +92,9 @@ def load_VMM_zrx_timeseries_from_ftp(server, login, password,
     """
     ftp = ftplib.FTP(server)
     ftp.login(login, password)
-    #discharge -> go to discharge folder
+    # discharge -> go to discharge folder
     ftp.cwd(path)
-    #check the files in the folder
+    # check the files in the folder
     files = ftp.nlst()
 
     # We're interested in the .zrx files, so let's get the information of
@@ -107,39 +107,39 @@ def load_VMM_zrx_timeseries_from_ftp(server, login, password,
         output = StringIO.StringIO()
         _gettext(ftp, zrxf, output)
 
-        #define header length (= number of lines with starting #)
+        # define header length (= number of lines with starting #)
         ctestall = StringIO.StringIO(output.getvalue())
         headerlength = 0
         ctest = '#'
         while ctest == '#':
             ctest = ctestall.readline()[0]
             headerlength += 1
-        print('File ',zrxf,' headerlength is: ', headerlength-1)
+        print('File ', zrxf, ' headerlength is: ', headerlength-1)
 
         temp = pd.read_table(StringIO.StringIO(output.getvalue()), sep=' ',
-                           skiprows=headerlength-1, index_col=0,
-                           parse_dates=True,
-                           header=None, usecols=(0, 1),
-                           names=['Time', zrxf[:-4]], na_values='-777.0')
+                             skiprows=headerlength-1, index_col=0,
+                             parse_dates=True,
+                             header=None, usecols=(0, 1),
+                             names=['Time', zrxf[:-4]], na_values='-777.0')
         data.append(temp)
         output.close()
     ftp.close()
 
-    ##CONCATENATE THE DIFFERENT DATAFRAMES IN ONE BIG:
+    # CONCATENATE THE DIFFERENT DATAFRAMES IN ONE BIG:
     data2 = pd.concat(data, axis=1)
 
-    ##SAVE THE DATA IN ONE PICKLE FOR NEXT TIME WORKING WITH THESE
+    # SAVE THE DATA IN ONE PICKLE FOR NEXT TIME WORKING WITH THESE
     data2.to_csv(datetime.datetime.now().strftime("%Y%m%d") +
-                    "VMM_" + dataname+".csv", float_format="%.3f",
-                    na_rep="Nan")
-    data2.to_pickle(datetime.datetime.now().strftime("%Y%m%d")
-                    + "VMM_" + dataname)
+                 "VMM_" + dataname+".csv", float_format="%.3f",
+                 na_rep="Nan")
+    data2.to_pickle(datetime.datetime.now().strftime("%Y%m%d") +
+                    "VMM_" + dataname)
 
 
 def _minutes2hours(minutes):
     """convert minutes to hours and return als rest
     """
-    return minutes/60, minutes%60
+    return minutes/60, minutes % 60
 
 
 def load_CEH_timeserie(filename):
@@ -160,9 +160,9 @@ def load_CEH_timeserie(filename):
                                               _minutes2hours(int(h))[0],
                                               _minutes2hours(int(h))[1])
     ceh_data = pd.read_csv(filename, sep=',',
-                       parse_dates={"Datetime":[1,2,3,4]},
-                       date_parser=parse,
-                       skipinitialspace=True)
+                           parse_dates={"Datetime": [1, 2, 3, 4]},
+                           date_parser=parse,
+                           skipinitialspace=True)
     ceh_data = ceh_data.set_index("Datetime")
 
     return ceh_data
--- setup.py
+++ setup.py
@@ -3,16 +3,17 @@
 from setuptools import setup
 
 setup(name='hydropy',
-	  version='0.1',
-	  description='Analysis of hydrological oriented time series',
-	  url='https://stijnvanhoey.github.io/hydropy/',
+      version='0.1',
+      description='Analysis of hydrological oriented time series',
+      url='https://stijnvanhoey.github.io/hydropy/',
       author='Stijn Van Hoey',
-	  author_email='stvhoey.vanhoey@ugent.be',
-	  license='BSD',
-       include_package_data=True,
-	  packages=['hydropy'],
-	  keywords='hydrology time series hydroTSM',
-       classifiers=[
+      author_email='stvhoey.vanhoey@ugent.be',
+      license='BSD',
+      include_package_data=True,
+      packages=['hydropy'],
+      keywords='hydrology time series hydroTSM',
+      test_suite='tests',
+      classifiers=[
         'Development Status :: 1 - Planning',
         'Intended Audience :: Science/Research',
         'License :: OSI Approved :: BSD License',
@@ -22,5 +23,4 @@ setup(name='hydropy',
         'Topic :: Scientific/Engineering :: Information Analysis',
         'Topic :: Scientific/Engineering :: Visualization',
         'Topic :: Utilities'
-    ],
-        test_suite='tests',)
+        ])
